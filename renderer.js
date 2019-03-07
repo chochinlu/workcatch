6dfbb4a6123b4dfa15d27b55d6fa9db8677280cb
Cho-Ching Lu <chochinlu@gmail.com>
2019-03-07 16:04:50 +0800

    Add screenshot

--- renderer.js
+++ renderer.js
@@ -1,3 +1,17 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+const screenshot = require("screenshot-desktop");
+
+const getScreenShot = () => {
+  screenshot({ filename: 'shot.jpg' })
+    .then(imgPath => {
+      // img: Buffer filled with jpg goodness
+      console.log(`catched! ${imgPath}`);
+    })
+    .catch(err => {
+      console.log("catch error!");
+    });
+};
+exports.getScreenShot = getScreenShot;
